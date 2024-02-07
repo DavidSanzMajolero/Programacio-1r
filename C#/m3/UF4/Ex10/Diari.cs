9b7a0a286f3897f1a936364381b268fcab8c3721
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2024-02-07 13:19:28 +0100

    Ex10

---
+++ C#/m3/UF4/Ex10/Diari.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Ex10
+
+{
+    public class Diari : Papel
+    {
+        public Diari(string nom, string editorial, string data, float volum, int numPag, string autor, string categoria) : base(nom, editorial, data, volum, numPag, autor, categoria) { }
+        public override bool Desplç()
+        {
+            return true;
+        }
+        public override bool Estripat()
+        {
+            return false;
+        }
+        public override string GetInfo()
+        {
+            return $"Nom: {Nom}, Editorial: {Editorial}, Data: {Data}, Volum: {Volum}, NumPag: {NumPag}, Autor: {Autor}, Categoria: {Categoria}";
+        }
+    }
+}
---
+++ C#/m3/UF4/Ex10/Prestar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Ex10
+{
+    public interface Prestar
+    {
+
+        public bool PrestarObj()
+        {
+            return true;
+        }
+        public bool Retornar()
+        {
+            return true;
+        }
+        public int TenirEnderreriment(int num)
+        {
+            return num;
+        }
+
+    }
+}
