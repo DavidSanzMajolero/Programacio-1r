99cec75bb653f347d6389aca1630b167b64e9c6a
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2024-02-19 13:44:22 +0100

    ex16

---
+++ C#/m3/UF4/Ex16/PaymentCard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Ex16
+{
+    public class PaymentCard
+    {
+        private double balance;
+
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+        public PaymentCard(double balance)
+        {
+            Balance = balance;
+        }
+
+        public override string ToString()
+        {
+            return "The card has a balance of " + balance + " pounds";
+        }
+    }
+}
+
---
+++ C#/m3/UF4/Ex16/Program.cs
@@ -0,0 +1,13 @@
+﻿using Ex16;
+namespace Ex16
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            PaymentCard card = new PaymentCard(50);
+            Console.WriteLine(card);
+        }
+
+    }
+}
