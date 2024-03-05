f5b42bbde2ee395e2f961d0514ce05dbafd6ea90
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2024-03-05 11:21:37 +0100

    ex5

---
+++ C#/m3/UF5/Ex5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+
+namespace Activitats
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DateTime date = DateTime.Now;
+            Console.WriteLine("Today is " + GetDayOfWeek(date));
+
+        }
+        public static string GetDayOfWeek(DateTime date)
+        {
+            return date.DayOfWeek.ToString();
+        }
+    }
+}
