c0487ec5d737970d2017e974c8641f3ca6d115f6
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2024-03-06 12:18:44 +0100

    ex11

---
+++ C#/m3/UF5/Ex11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Activitats
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Dictionary<string, int> employeeDic = new Dictionary<string, int>();
+            employeeDic.Add("David", 13123);
+            employeeDic.Add("Eric", 23234);
+            employeeDic.Add("Molanito", 21212);
+            foreach (var item in employeeDic)
+            {
+                Console.WriteLine($"{item.Key} {item.Value}");
+            }
+
+        }
+    }
+}
