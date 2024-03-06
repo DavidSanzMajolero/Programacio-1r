791b17dd91e90f025824eb58f8c32630bb68d1d7
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2024-03-06 12:02:57 +0100

    ex8

---
+++ C#/m3/UF5/Ex8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Activitats
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<int> nums = new List<int>
+            {
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10
+            };
+            int sum = 0;
+            foreach (int num in nums)
+            {
+                sum += num;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
