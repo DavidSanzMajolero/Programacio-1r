bd178e6f7504c358e46c3471d739999fcdd6c0df
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2024-03-06 13:19:08 +0100

    ex15

---
+++ C#/m3/UF5/Ex15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace Activitats
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Regex regex = new Regex(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
+            string email;
+            do
+            {
+                Console.WriteLine("Introdueix el teu email: ");
+                email = Console.ReadLine();
+            } while (!regex.IsMatch(email));
+        }
+    }
+}
