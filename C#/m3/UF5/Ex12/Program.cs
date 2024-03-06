5e54484de1a660dc5ca9c668379b23dfba5946a5
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2024-03-06 12:34:14 +0100

    ex12

---
+++ C#/m3/UF5/Ex12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Activitats
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<int> numbers = new List<int> { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            int specificValue = 50;
+            numbers.RemoveAll(num => num > specificValue);
+
+            Console.WriteLine("Llista després d'eliminar els elements majors que el valor específic:");
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
