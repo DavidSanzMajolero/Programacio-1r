d41d81c46ca4c0bcad427a1534f7480da3e3a7a0
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2024-03-06 12:11:39 +0100

    ex10

---
+++ C#/m3/UF5/Ex10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Activitats
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] strings = new string[] { "david", "arnau", "eric", "asdd" };
+
+            List<string> stringList = new List<string>(strings);
+
+            foreach (string str in stringList)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
