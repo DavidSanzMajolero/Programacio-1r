8f2f44fefdc8b6913660f25f5be0fb62d417de68
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2024-03-13 13:45:02 +0100

    ex20

---
+++ C#/m3/UF5/Ex20/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Activitats
+{
+    public class Program
+    {
+        public delegate double MyDelegate(int x);
+        public static void Main()
+        {
+            MyDelegate number = Square;
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number(x));
+        }
+        public static double Square(int x)
+        {
+            return Math.Sqrt(x);
+        }
+    }
+}
