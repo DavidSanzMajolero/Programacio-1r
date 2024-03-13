aa65bd23c2a442e28a5e9c2c92debd67a73cf685
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2024-03-13 13:53:08 +0100

    Ex21

---
+++ C#/m3/UF5/Ex21/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Activitats
+{
+    public class Program
+    {
+        public delegate double MyDelegate(int[] x);
+        public static void Main()
+        {
+            MyDelegate num = Average;
+            int[] x =  [ 5,4,4,4,4,5,8,1 ];
+            Console.WriteLine(num(x));
+        }
+        public static double Average(int[] x)
+        {
+            int result = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                result += x[i];
+            }
+            return result / x.Length;
+        }
+    }
+}
