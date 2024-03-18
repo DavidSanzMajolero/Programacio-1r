e4d8b9b857d8815f5f4ee2ce9cb00d5e48e54dc4
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2024-03-18 12:33:22 +0100

    ex22

---
+++ C#/m3/UF5/Ex22/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Activitats
+{
+    public class Program
+    {
+        public delegate double MyDelegate(int x);
+        public static void Main()
+        {
+            MyDelegate num = EvenOdd;
+            int x = Convert.ToInt32(Console.ReadLine());
+            if (num(x) == 5)
+            {
+                Console.WriteLine("El número es impar");
+            }
+            else
+            {
+                Console.WriteLine(num(x));
+            }
+        }
+        public static double EvenOdd(int x)
+        {
+            if (x % 2 == 0)
+            {
+                return x * 2;
+            }
+            else
+            {
+                return 5;
+            }
+        }
+
+    }
+}
