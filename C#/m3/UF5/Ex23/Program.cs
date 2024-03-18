51d1833ddd06de1a69a86eea7088271d037ca481
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2024-03-18 13:06:00 +0100

    ex23

---
+++ C#/m3/UF5/Ex23/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Activitats
+{
+
+    public delegate int FibonacciDelegate(int n);
+
+    public class Program
+    {
+        static void Main()
+        {
+            FibonacciDelegate fibonacciDelegate = FibonacciCalculator.Fibonacci;
+
+            int position = 5;
+
+            int result = fibonacciDelegate(position);
+
+            Console.WriteLine($"El número Fibonacci de la posició {position} és: {result}");
+        }
+    }
+    public class FibonacciCalculator
+    {
+        public static int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
