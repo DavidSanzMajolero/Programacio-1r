673a49592d85a8fd9cd430d93d76e2e47390845a
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2024-03-20 12:58:03 +0100

    EasterEGG

---
+++ C#/m3/UF5/borrar/Character.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using EasterEggCharacter;
+using System;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace EasterEggCharacter
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int HP { get; set; }
+        public int Atack {  get; set; }
+        public int Defense { get; set;}
+
+        public Character(int id, string name, int level, int hp, int atack, int defense)
+        {
+            Id = id;
+            Name = name;
+            Level = level;
+            HP = hp;
+            Atack = atack;
+            Defense = defense;
+        }
+    }
+}
---
+++ C#/m3/UF5/borrar/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using EasterEggCharacter;
+using System;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace EasterEgg
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            const string RabbitArt = @"      / \
+                                            / _ \
+                                           | / \ |
+                                           ||   || _______
+                                           ||   || |\     \
+                                           ||   || ||\     \
+                                           ||   || || \    |
+                                           ||   || ||  \__/
+                                           ||   || ||   ||
+                                            \\_/ \_/ \_//
+                                           /   _     _   \
+                                          /               \
+                                          |    O     O    |
+                                          |   \  ___  /   |                           
+                                         /     \ \_/ /     \
+                                        /  -----  |  --\    \
+                                        |     \__/|\__/ \   |
+                                        \       |_|_|       /
+                                         \_____       _____/
+                                               \     /
+                                               |     |";
+            const string Menu = "EasterEgg:\n" + "[1]CREAR PERSONATGE \n " + "[2]LLEGIR PERSONATGES \n " + "[3]ACTUALITZAR PERSONATGES \n ";
+            string name = " ";
+            int function;
+            int id = 1, level = 1, hp = 50, atack = 20, defense = 25;
+
+            Console.WriteLine(RabbitArt);
+            Console.WriteLine(Menu);
+            //do
+            //{
+                function = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+            //} while (function != 1 || function != 2 || function != 3 );
+
+            switch (function)
+            {
+                case 1:
+                    Console.WriteLine("Introdueix el nom del personatge: ");
+                    name = Console.ReadLine();
+                    Character characterOne = new Character(id, name, level, hp, atack, defense);
+                    XMLHelpers.CreateXMLFileWithLINQ(characterOne);
+
+                    break;
+
+                case 2:
+                    XMLHelpers.ReadXMLFileWithLINQ();
+                    break;
+
+                case 3:
+                    
+                    //ACTUALITZAR PERSONATGES XMLHELPER
+                    break;
+
+                default:
+
+                    break;
+
+            }
+        }
+    }
+}
+
